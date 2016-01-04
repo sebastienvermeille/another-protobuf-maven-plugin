b4d7a8d171875b1d6bf647f62ede9c460ce96d62
Sergei Ivanov <sergei@xolstice.org>
2016-01-04 02:08:21 +0000

    Changed GAV for all artifacts

--- src/it/TEST-15/verify.groovy
+++ src/it/TEST-15/verify.groovy
@@ -2,7 +2,7 @@ outputDirectory = new File(basedir, 'target/generated-resources/protobuf/descrip
 assert outputDirectory.exists();
 assert outputDirectory.isDirectory();
 
-generatedJavaFile = new File(outputDirectory, 'TEST-15-0.0.1-SNAPSHOT.protobin');
+generatedJavaFile = new File(outputDirectory, 'test-15-1.0.0.protobin');
 assert generatedJavaFile.exists();
 assert generatedJavaFile.isFile();
 
--- src/it/TEST-24/verify.groovy
+++ src/it/TEST-24/verify.groovy
@@ -2,7 +2,7 @@ outputDirectory = new File(basedir, 'project2/target/dependency');
 assert outputDirectory.exists();
 assert outputDirectory.isDirectory();
 
-generatedJavaFile = new File(outputDirectory, 'TEST-24-project1-0.0.1-SNAPSHOT.protobin');
+generatedJavaFile = new File(outputDirectory, 'test-24-project1-1.0.0.protobin');
 assert generatedJavaFile.exists();
 assert generatedJavaFile.isFile();
 
